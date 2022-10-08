810e9dc42aa312d09188c5720238461cb8ddd72f
Greg Tracy <gtracy@gmail.com>
2022-10-07 20:43:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const config = require('./config');
+const express = require('express');
+const pino = require('pino-http')(config.getLogConfig());
+
+// request logger
+let logger = (req,res,next) => {
+    req.log.info(req.url,'new request');
+    next();
+}
+
+const app = express();
+app.use(pino);
+app.use(logger);
+
+// API endpoint registration
+
+// API backstop
+app.get('*', (req,res) => {
+    res.json({
+        "status": -1,
+        "description": 'unsupported endpoint'
+    });
+});
+
+// error handler
+app.use( (err, req, res, next) => {
+    res.json({
+        "status": -1,
+        "description": err
+    });
+});
+
+module.exports = app;
